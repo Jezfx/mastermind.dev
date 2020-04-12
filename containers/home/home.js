52f96dfabe7fe847211acd54655a53aef76e21af
jezfx <jez@jezfx.com>
2020-04-12 23:44:40 +0100

    fix broken image

---
+++ containers/home/home.js
@@ -0,0 +1,56 @@
+import Layout from "~/components/layout/"
+import Button from "~/components/button/"
+import { Header } from "~/components/typography/"
+
+import * as Styles from "./home.styles"
+import * as Constants from "./home.constants"
+
+export default () => (
+  <Layout>
+    <Styles.Container>
+      <Styles.Main>
+        <Styles.Intro>
+          Meet on a regular basis with other driven developers to share ideas,
+          learn and teach.
+        </Styles.Intro>
+
+        <Styles.ButtonWrapper mobile>
+          <Button>{Constants.buttonText}</Button>
+        </Styles.ButtonWrapper>
+
+        <Header as="h3">Goal</Header>
+        <p>
+          As a developer, it's hard to stay on top of how quick the industry
+          moves and keeping up with news, best practices and .
+        </p>
+        <p>
+          The goal of these masterminds is for small groups to work together to
+          share essential knowledge to build each other up.
+        </p>
+
+        <Header as="h3">How it works</Header>
+        <p>
+          Tell us about your background, goals and interests and we'll put you
+          in a group (max 5) of similar skills sets and levels to meet on a
+          regular basis.
+        </p>
+        <p>
+          Once a month you'll get together for your mastermind to share
+          something interesting you've learnt or discovered that month. A new
+          person each month will lead the group to manage time and structure.
+        </p>
+      </Styles.Main>
+      <Styles.Aside>
+        <>
+          <Styles.Image
+            src="./images/conferance-call.svg"
+            alt="conferance call"
+          />
+          <Styles.ButtonWrapper desktop>
+            <Button>{Constants.buttonText}</Button>
+          </Styles.ButtonWrapper>
+        </>
+      </Styles.Aside>
+    </Styles.Container>
+  </Layout>
+)
--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fd4c40c018024127f731";
+/******/ 	var hotCurrentHash = "919296db3d0eff590cf8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
