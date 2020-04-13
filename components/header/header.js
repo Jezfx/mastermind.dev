6c93eedb2f0eaddaf75512f6bc3066aa21c05564
jezfx <jez@jezfx.com>
2020-04-13 12:07:31 +0100

    add favicon

--- components/header/header.js
+++ components/header/header.js
@@ -3,7 +3,7 @@ import * as Styles from "./header.styles"
 export default function Header() {
   return (
     <header>
-      <Styles.Logo>mastermind</Styles.Logo>
+      <Styles.Logo>mastermind ✨</Styles.Logo>
       <Styles.SubHeader>
         Virtual mastermind groups for developers.
       </Styles.SubHeader>
